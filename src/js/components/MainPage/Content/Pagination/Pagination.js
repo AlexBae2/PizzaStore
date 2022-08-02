94d81ec90aa1a8cb7323fd44fce051eb4c0174ed
AlexBae <pirtxxei@gmail.com>
2022-08-03 01:31:28 +0500

    Minor changes

--- src/js/components/MainPage/Content/Pagination/Pagination.js
+++ src/js/components/MainPage/Content/Pagination/Pagination.js
@@ -1,23 +1,28 @@
 import classes from "./Pagination.module.css";
-import {useDispatch, useSelector} from "react-redux";
-import {setCurrentPage} from "../../../../redux/contentReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentPage } from "../../../../redux/contentReducer";
 
 const Pagination = () => {
-    const dispatch = useDispatch();
-    const totalPizzasCount = useSelector((state) => state.main.totalPizzasCount);
-    const switchPage = (page) => {
-        dispatch(setCurrentPage({page}));
-    }
+  const dispatch = useDispatch();
+  const totalPizzasCount = useSelector((state) => state.main.totalPizzasCount);
+  const switchPage = (page) => {
+    dispatch(setCurrentPage({ page }));
+  };
+  const b = 0;
 
-    return (
-        <div className={classes.paginationWrapper}>
-            <div className={classes.pagination}>
-                <ul className={classes.paginationList}>
-                    {[...Array(totalPizzasCount).keys()].map((item, indexItem) => <li onClick={() => switchPage(indexItem+1)} key={item}>{indexItem + 1}</li>)}
-                </ul>
-            </div>
-        </div>
-    )
+  return (
+    <div className={classes.paginationWrapper}>
+      <div className={classes.pagination}>
+        <ul className={classes.paginationList}>
+          {[...Array(totalPizzasCount).keys()].map((item, indexItem) => (
+            <li onClick={() => switchPage(indexItem + 1)} key={item}>
+              {indexItem + 1}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Pagination;
